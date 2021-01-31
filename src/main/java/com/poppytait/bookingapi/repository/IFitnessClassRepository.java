1112b20daa1298ea6adf9b73ce58ea47da2963e5
Poppy Tait <poppytait@gmail.com>
2021-01-31 15:29:46 +0100

    Add fitness class repository

---
+++ src/main/java/com/poppytait/bookingapi/repository/IFitnessClassRepository.java
@@ -0,0 +1,8 @@
+package com.poppytait.bookingapi.repository;
+
+import com.poppytait.bookingapi.model.FitnessClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FitnessClassRepository extends CrudRepository<FitnessClass, Long> {
+
+}
